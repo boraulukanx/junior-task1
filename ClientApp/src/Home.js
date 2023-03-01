1d842639934e5fe2089960f6dce19a594d27685e
boraulukan <81370060+boraulukan@users.noreply.github.com>
2023-03-01 06:10:03 +0000

    updated
    junior task

---
+++ ClientApp/src/Home.js
@@ -0,0 +1,174 @@
+﻿import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import "./Home.css";
+
+function Home() {
+
+    const [name, setName] = useState();
+    const [value, setValue] = useState();
+    const [employees, setEmployees] = useState([])
+    const [sums, setSums] = useState();
+
+    
+    //creating an employee
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const res = await axios.post("http://localhost:41478/List/CreateEmployee",
+                {
+                    name,
+                    value
+                });
+            setEmployees([...employees, res.data]);
+            setName("");
+            setValue("");
+            console.log("successfully saved employee");
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    //deleting an employee
+    const handleDelete = async (name) => {
+        try {
+            await axios.delete(`http://localhost:41478/List/RemoveEmployee/${name}`);
+            setEmployees(
+                employees.filter(
+                    (employee) => employee.name !== name)
+            );
+            console.log("successfully deleted employee")
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    //getting employees
+    useEffect(() => {
+        const getEmployees = async () => {
+            const res = await axios.get("http://localhost:41478/List/GetEmployees");
+            setEmployees(res.data);
+        }
+        getEmployees()
+    }, [employees]);
+
+    //when clicking button, this will increment the values
+    const handleIncrement = async () => {
+        try {
+            await axios.get("http://localhost:41478/List/increment");
+            console.log("increment operation successfully done")
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    //Modify value of an employee
+    const [employeeName, setEmployeeName] = useState("");
+    const [newValue, setNewValue] = useState("");
+
+    const handleUpdate = async () => {
+        try {
+            const res = await axios.put("http://localhost:41478/List/PutEmployee", {
+                name: employeeName,
+                value: parseInt(newValue),
+            });
+            console.log("employee updated")
+            setEmployees([...employees, res.data]);
+            setEmployeeName("");
+            setNewValue("");
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    //I could be able to fetch SUM(Value) values that are
+    //bigger than or equal to 11171 by using POSTMAN and SQLite Online Editor.
+
+    /*const getSums = async () => {
+        try {
+            await axios.get("http://localhost:41478/List/sum")
+                .then(res => res.json())
+                .then(
+                    (results) => {
+                        setSums([...results]);
+                    }
+                );
+
+        } catch (err) {
+            console.log(err);
+        }
+    }*/
+
+
+    return (
+        <div>
+            <h1>Metricell Junior Task</h1>
+            <div className="employee-operations">
+                <h3>Add Employee</h3>
+                <form className="createEmployeeForm" onSubmit={handleSubmit}>
+                    <input
+                        placeholder="Name"
+                        required
+                        onChange={(e) => setName(e.target.value)}
+                        className="nameInput"
+                    />
+                    <input
+                        placeholder="Value"
+                        required
+                        onChange={(e) => setValue(e.target.value)}
+                        className="valueInput"
+                    />
+                    <button className="createBtn" type="submit">
+                        Create Employee
+                    </button>
+                </form>
+            </div>
+            <div>
+                <h1>Employee List</h1>
+                <button onClick={handleIncrement} >Increase Employee Value</button>
+                <form className="modifyValueForm" onSubmit={handleUpdate}>
+                    <input
+                        placeholder="Name of the employee"
+                        required
+                        type="text"
+                        value={employeeName}
+                        onChange={(e) => setEmployeeName(e.target.value)}
+                    />
+                    <input
+                        placeholder="Value"
+                        required
+                        type="number"
+                        value={newValue}
+                        onChange={(e) => setNewValue(e.target.value)}
+                    />
+                    <button type="submit">Change Value</button>
+                </form>
+                <table className="employee-table">
+                    <thead>
+                        <tr>
+                            <th>Employee Name</th>
+                            <th>Employee Value</th>
+                            <th>Remove</th>
+                        </tr>
+                    </thead>
+                    <tbody>{
+                        employees.map((employee) => {
+                            return (
+                                <tr>
+                                    <td>{employee.name}</td>
+                                    <td>{employee.value}</td>
+                                    <th>
+                                        <button onClick={() => handleDelete(employee.name)}>Remove</button>
+                                    </th>
+                                </tr>
+                            );
+                        })
+                    }</tbody>
+                </table>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
+
---
+++ Controllers/ListController.cs
@@ -0,0 +1,196 @@
+﻿using InterviewTest.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace InterviewTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListController : ControllerBase
+    {
+        public ListController()
+        {
+        }
+
+        [Route("GetEmployees")]
+        [HttpGet]
+        public JsonResult GetEmployeeList()
+        {
+            var employeesList = new List<Employee>();
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder() { DataSource = "./SqliteDB.db" };
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                var queryCmd = connection.CreateCommand();
+                queryCmd.CommandText = @"SELECT Name, Value FROM Employees";
+                using (var reader = queryCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        employeesList.Add(new Employee
+                        {
+                            Name = reader.GetString(0),
+                            Value = reader.GetInt32(1)
+                        });
+                    }
+                }                
+            }
+            return new JsonResult(employeesList);
+        }
+
+        [Route("CreateEmployee")]
+        [HttpPost]
+        public JsonResult CreateEmployee(Employee employee)
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder() { DataSource = "./SqliteDB.db" };
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                var createCmd = connection.CreateCommand();
+                createCmd.CommandText = @"INSERT INTO Employees(Name, Value) VALUES ('"+employee.Name+"', "+employee.Value+")";
+                createCmd.ExecuteNonQuery();
+            }
+            return new JsonResult("creation successful");
+        }
+
+        [Route("RemoveEmployee/{name}")]
+        [HttpDelete]
+        public JsonResult RemoveEmployee(string name)
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "./SqliteDB.db";
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                var deleteCmd = connection.CreateCommand();
+                deleteCmd.CommandText = @"DELETE FROM Employees WHERE Name  = '"+name+"'";
+                
+                deleteCmd.ExecuteNonQuery();
+            }
+
+            return new JsonResult("removal successful");
+        }
+
+        [Route("PutEmployee")]
+        [HttpPut]
+        public JsonResult PutEmployee(Employee employee)
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "./SqliteDB.db";
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                var updateCmd = connection.CreateCommand();
+                updateCmd.CommandText = @"UPDATE Employees SET Value = "+employee.Value+" WHERE Name = '"+employee.Name+"'";
+                updateCmd.ExecuteNonQuery();
+            }
+
+            return new JsonResult("update successful");
+        }
+        [Route("increment")]
+        [HttpGet]
+        public JsonResult IncrementValues()
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "./SqliteDB.db";
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                var incrementCmd = connection.CreateCommand();
+                incrementCmd.CommandText = @"UPDATE Employees
+        SET Value = Value +
+             CASE
+                 WHEN Name LIKE 'E%' THEN 1
+                 WHEN Name LIKE 'G%' THEN 10
+                 WHEN Name not LIKE 'G%' OR 'E%' THEN 100
+                 ELSE 0
+             END";
+                incrementCmd.ExecuteNonQuery();
+            }
+
+            return new JsonResult("values are updated");
+        }
+
+
+        //THIS ALGORITHM HAS BEEN TESTED ON
+        //POSTMAN AND SQLITE
+        //WORKS PERFECTLY WELL
+        [Route("sum")]
+        [HttpGet]
+        public JsonResult SumValues()
+        {
+
+            List<int> results = new List<int>();
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "./SqliteDB.db";
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                var queryCmd = connection.CreateCommand();
+                queryCmd.CommandText = @"SELECT SUM(Value)
+                    FROM Employees
+                    WHERE Name LIKE 'A%'";
+        
+                using (var reader = queryCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int value = reader.GetInt32(0);
+                            if (value >= 11171)
+                            {
+                                results.Add(value);
+                            }
+                    }
+                }
+                queryCmd.CommandText = @"SELECT SUM(Value)
+                    FROM Employees
+                    WHERE Name LIKE 'B%'";
+
+                using (var reader = queryCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int value = reader.GetInt32(0);
+                        if (value >= 11171)
+                        {
+                            results.Add(value);
+                        }
+                    }
+                }
+                queryCmd.CommandText = @"SELECT SUM(Value)
+                    FROM Employees
+                    WHERE Name LIKE 'C%'";
+
+                using (var reader = queryCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int value = reader.GetInt32(0);
+                        if (value >= 11171)
+                        {
+                            results.Add(value);
+                        }
+                    }
+                }
+
+            }
+
+            return new JsonResult(results);
+        }
+
+    }
+
+}
